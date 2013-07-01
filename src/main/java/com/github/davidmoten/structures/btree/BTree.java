127f0294449652bab8d8217a5d44ca85e22b8dd9
Dave Moten <davidmoten@gmail.com>
2013-07-01 19:20:07 +1000

    implement iterator

--- src/main/java/com/github/davidmoten/structures/btree/BTree.java
+++ src/main/java/com/github/davidmoten/structures/btree/BTree.java
@@ -10,109 +10,119 @@ import com.google.common.annotations.VisibleForTesting;
 
 public class BTree<T extends Comparable<T>> implements Iterable<T> {
 
-    private Node<T> root;
+	private Node<T> root;
 
-    public BTree(int degree) {
-        root = new Node<T>(degree);
-    }
+	public BTree(int degree) {
+		root = new Node<T>(degree);
+	}
 
-    public void add(T key) {
-        Node<T> newRoot = root.add(key);
-        if (newRoot != null)
-            root = newRoot;
-    }
+	public void add(T key) {
+		Node<T> newRoot = root.add(key);
+		if (newRoot != null)
+			root = newRoot;
+	}
 
-    @VisibleForTesting
-    List<? extends Key<T>> getKeys() {
-        return root.getKeys();
-    }
+	@VisibleForTesting
+	List<? extends Key<T>> getKeys() {
+		return root.getKeys();
+	}
 
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("BTree [root=");
-        builder.append(root);
-        builder.append("]");
-        return builder.toString();
-    }
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("BTree [root=");
+		builder.append(root);
+		builder.append("]");
+		return builder.toString();
+	}
 
-    private static enum Side {
-        LEFT, RIGHT;
-    }
+	private static enum Side {
+		LEFT, RIGHT;
+	}
 
-    private final static class NodeIndex<R extends Comparable<R>> {
-        public NodeIndex(Node<R> node, int index, Side side) {
-            super();
-            this.node = node;
-            this.index = index;
-            this.side = side;
-        }
+	private final static class NodeIndex<R extends Comparable<R>> {
+		public NodeIndex(Node<R> node, int index, Side side) {
+			super();
+			this.node = node;
+			this.index = index;
+			this.side = side;
+		}
 
-        final Node<R> node;
-        final int index;
-        final Side side;
+		final Node<R> node;
+		final int index;
+		final Side side;
 
-        public Key<R> getKey() {
-            return node.getKey(index);
-        }
-    }
+		public Key<R> getKey() {
+			return node.getKey(index);
+		}
+	}
 
-    @Override
-    // TODO implement this
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
+	@Override
+	// TODO implement this
+	public Iterator<T> iterator() {
+		return new Iterator<T>() {
 
-            private final Deque<NodeIndex<T>> stack = new LinkedList<NodeIndex<T>>();
+			private final Deque<NodeIndex<T>> stack = new LinkedList<NodeIndex<T>>();
 
-            {
-                stack.add(new NodeIndex<T>(root, 0, Side.LEFT));
-            }
+			{
+				stack.add(new NodeIndex<T>(root, 0, Side.LEFT));
+			}
 
-            @Override
-            public boolean hasNext() {
-                return !stack.isEmpty();
-            }
+			@Override
+			public boolean hasNext() {
+				return !stack.isEmpty();
+			}
 
-            @Override
-            public T next() {
-                // should return the current value pointed at by
-                // stack.peekLast();
-                if (stack.isEmpty())
-                    throw new NoSuchElementException();
-                NodeIndex<T> n = stack.peekLast();
-                T value = n.getKey().value();
-                if (!n.getKey().hasChild()) {
-                    // leaf node
-                    if (n.index < n.node.getKeys().size() - 1) {
-                        stack.pop();
-                        stack.push(new NodeIndex<T>(n.node, n.index + 1,
-                                Side.LEFT));
-                    } else if (n.index == n.node.getKeys().size() - 1
-                            && n.side == Side.LEFT) {
-                        stack.pop();
-                        stack.push(new NodeIndex<T>(n.node, n.index, Side.RIGHT));
-                    } else {
-                        stack.pop();
-                    }
-                } else {
-                    if (n.side == Side.LEFT
-                            && n.index < n.node.getKeys().size() - 1) {
-                        stack.push(new NodeIndex<T>(n.getKey().getLeft(), 0,
-                                Side.LEFT));
-                        return next();
-                    } else if (n.side == Side.LEFT
-                            && n.index == n.node.getKeys().size() - 1) {
+			@Override
+			public T next() {
+				// should return the current value pointed at by
+				// stack.peekLast();
 
-                    }
-                }
-                return value;
+				if (stack.isEmpty())
+					throw new NoSuchElementException();
+				NodeIndex<T> n = stack.peekLast();
+				if (!n.getKey().hasChild()) {
+					T value = n.getKey().value();
+					// leaf node
+					if (n.index < n.node.getKeys().size() - 1) {
+						stack.pop();
+						stack.push(new NodeIndex<T>(n.node, n.index + 1,
+								Side.LEFT));
+					} else if (n.index == n.node.getKeys().size() - 1
+							&& n.side == Side.LEFT) {
+						stack.pop();
+						stack.push(new NodeIndex<T>(n.node, n.index, Side.RIGHT));
+					} else {
+						stack.pop();
+					}
+					return value;
+				} else {
+					if (n.side == Side.LEFT
+							&& n.index < n.node.getKeys().size() - 1) {
+						stack.pop();
+						stack.push(new NodeIndex<T>(n.node, n.index + 1,
+								Side.LEFT));
+						stack.push(new NodeIndex<T>(n.getKey().getLeft(), 0,
+								Side.LEFT));
+					} else if (n.side == Side.LEFT
+							&& n.index == n.node.getKeys().size() - 1) {
+						stack.pop();
+						stack.push(new NodeIndex<T>(n.node, n.index, Side.RIGHT));
+					} else {
+						// side == Side.RIGHT
+						stack.pop();
+						stack.push(new NodeIndex<T>(n.getKey().getRight(), 0,
+								Side.LEFT));
+					}
+					return next();
+				}
 
-            }
+			}
 
-            @Override
-            public void remove() {
-                // TODO
-            }
-        };
-    }
+			@Override
+			public void remove() {
+				// TODO
+			}
+		};
+	}
 }
