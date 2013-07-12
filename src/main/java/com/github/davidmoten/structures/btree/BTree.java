35336b504d070c63c489dcdf510f9804eadcf05d
Dave Moten <davidmoten@gmail.com>
2013-07-12 17:08:39 +1000

    add javadoc

--- src/main/java/com/github/davidmoten/structures/btree/BTree.java
+++ src/main/java/com/github/davidmoten/structures/btree/BTree.java
@@ -19,16 +19,48 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 
+/**
+ * A standard BTree implementation as per wikipedia entry with some tweaks to
+ * add in concurrency. In particular, iteration through the btree can be done
+ * concurrently with addition/deletion with the side-effect that the iteration
+ * might include changes to the tree since the iterator was created.
+ * 
+ * @author dxm
+ * 
+ * @param <T>
+ */
 public class BTree<T extends Serializable & Comparable<T>> implements
 		Iterable<T> {
 
+	/**
+	 * The root node.
+	 */
 	private Node<T> root;
 
+	/**
+	 * The maximum number of keys in a node plus one.
+	 */
 	private final int degree;
+
+	/**
+	 * The file the btree is persisted to.
+	 */
 	private final Optional<File> file;
+
+	/**
+	 * The maximum number of bytes required to serialize T.
+	 */
 	private final int keySize;
-	private final static long POSITION_START = 1000;
-	private long rootPosition = POSITION_START;
+
+	/**
+	 * Where node storage starts in the file.
+	 */
+	private final static long NODE_STORAGE_START = 1000;
+
+	/**
+	 * The current position in the file of the root node.
+	 */
+	private long rootPosition = NODE_STORAGE_START;
 	/**
 	 * Manages allocation of file positions for nodes.
 	 */
@@ -47,6 +79,8 @@ public class BTree<T extends Serializable & Comparable<T>> implements
 	 */
 	private BTree(int degree, Optional<File> file, Class<T> cls, int keySize) {
 		Preconditions.checkArgument(degree >= 2, "degree must be >=2");
+		Preconditions.checkArgument(keySize > 0, "keySize must be >0");
+		Preconditions.checkNotNull(file, "file cannot be null");
 		this.degree = degree;
 		this.file = file;
 		this.positionManager = new PositionManager(file);
@@ -61,10 +95,14 @@ public class BTree<T extends Serializable & Comparable<T>> implements
 		}
 	}
 
+	/**
+	 * Reads the header information from the file including the position of the
+	 * root node.
+	 */
 	private void readHeader() {
 		try {
 			RandomAccessFile f = new RandomAccessFile(file.get(), "r");
-			byte[] header = new byte[(int) POSITION_START];
+			byte[] header = new byte[(int) NODE_STORAGE_START];
 			f.seek(0);
 			f.read(header);
 			f.close();
@@ -79,20 +117,24 @@ public class BTree<T extends Serializable & Comparable<T>> implements
 		}
 	}
 
+	/**
+	 * Writes the header information to the file including the position of the
+	 * root node.
+	 */
 	private synchronized void writeHeader() {
 		try {
 			if (!file.get().exists())
 				file.get().createNewFile();
 			RandomAccessFile f = new RandomAccessFile(file.get(), "rws");
 			ByteArrayOutputStream header = new ByteArrayOutputStream(
-					(int) POSITION_START);
+					(int) NODE_STORAGE_START);
 			ObjectOutputStream oos = new ObjectOutputStream(header);
 			oos.writeLong(rootPosition);
 			oos.close();
 			f.seek(0);
 			f.write(header.toByteArray());
-			if (header.size() < POSITION_START) {
-				byte[] more = new byte[(int) POSITION_START - header.size()];
+			if (header.size() < NODE_STORAGE_START) {
+				byte[] more = new byte[(int) NODE_STORAGE_START - header.size()];
 				f.write(more);
 			}
 			f.close();
@@ -103,47 +145,93 @@ public class BTree<T extends Serializable & Comparable<T>> implements
 		}
 	}
 
+	/**
+	 * Builder for a {@link BTree}.
+	 * 
+	 * @author dxm
+	 * 
+	 * @param <R>
+	 */
 	public static class Builder<R extends Serializable & Comparable<R>> {
 		private int degree = 100;
 		private Optional<File> file = absent();
 		private int keySize = 1000;
 		private final Class<R> cls;
 
+		/**
+		 * Constructor.
+		 * 
+		 * @param cls
+		 */
 		public Builder(Class<R> cls) {
 			this.cls = cls;
 		}
 
+		/**
+		 * Sets the degree.
+		 * 
+		 * @param degree
+		 * @return
+		 */
 		public Builder<R> degree(int degree) {
 			this.degree = degree;
 			return this;
 		}
 
+		/**
+		 * Sets the file.
+		 * 
+		 * @param file
+		 * @return
+		 */
 		public Builder<R> file(File file) {
 			this.file = of(file);
 			return this;
 		}
 
+		/**
+		 * Sets the keySize.
+		 * 
+		 * @param keySize
+		 * @return
+		 */
 		public Builder<R> keySize(int keySize) {
 			this.keySize = keySize;
 			return this;
 		}
 
+		/**
+		 * Returns a new {@link BTree}.
+		 * 
+		 * @return
+		 */
 		public BTree<R> build() {
 			return new BTree<R>(degree, file, cls, keySize);
 		}
 	}
 
+	/**
+	 * Creates a {@link Builder}.
+	 * 
+	 * @param cls
+	 * @return
+	 */
 	public static <R extends Comparable<R> & Serializable> Builder<R> builder(
 			Class<R> cls) {
 		return new Builder<R>(cls);
 	}
 
+	/**
+	 * Returns the degree (the max number of keys in a node plus one).
+	 * 
+	 * @return
+	 */
 	public int getDegree() {
 		return degree;
 	}
 
 	/**
-	 * Adds an element to the b-tree. May replace root.
+	 * Adds one or more elements to the b-tree. May replace root.
 	 * 
 	 * @param t
 	 */
@@ -153,6 +241,11 @@ public class BTree<T extends Serializable & Comparable<T>> implements
 		return this;
 	}
 
+	/**
+	 * Adds a single element to the btree.
+	 * 
+	 * @param t
+	 */
 	private void addOne(T t) {
 		synchronized (writeMonitor) {
 			Optional<Node<T>> newRoot = root.add(t,
@@ -190,6 +283,13 @@ public class BTree<T extends Serializable & Comparable<T>> implements
 		return null;
 	}
 
+	/**
+	 * Deletes (or marks as deleted) those keys in the BTree that match one of
+	 * the keys in the parameter.
+	 * 
+	 * @param keys
+	 * @return
+	 */
 	public long delete(T... keys) {
 		long count = 0;
 		for (T key : keys)
@@ -197,12 +297,52 @@ public class BTree<T extends Serializable & Comparable<T>> implements
 		return count;
 	}
 
+	/**
+	 * Deletes (or marks as deleted) all keys in the BTree that equal
+	 * <code>key</code>.
+	 * 
+	 * @param key
+	 * @return
+	 */
 	private long deleteOne(T key) {
 		synchronized (writeMonitor) {
 			return root.delete(key);
 		}
 	}
 
+	/**
+	 * Returns the file the btree is being persisted to. Returns
+	 * Optional.absent() if none defined.
+	 * 
+	 * @return
+	 */
+	public Optional<File> getFile() {
+		return file;
+	}
+
+	/**
+	 * Returns the key size in bytes.
+	 * 
+	 * @return
+	 */
+	public int getKeySize() {
+		return keySize;
+	}
+
+	/**
+	 * Returns the {@link PositionManager} for this btree.
+	 * 
+	 * @return
+	 */
+	PositionManager getPositionManager() {
+		return positionManager;
+	}
+
+	/**
+	 * Returns the keys as a {@link List}.
+	 * 
+	 * @return
+	 */
 	@VisibleForTesting
 	List<? extends Key<T>> getKeys() {
 		return root.getKeys();
@@ -220,18 +360,6 @@ public class BTree<T extends Serializable & Comparable<T>> implements
 		builder.append(root);
 		builder.append("]");
 		return builder.toString();
-	}
-
-	public Optional<File> getFile() {
-		return file;
-	}
-
-	public int getKeySize() {
-		return keySize;
-	}
-
-	public PositionManager getPositionManager() {
-		return positionManager;
 	}
 
 }
