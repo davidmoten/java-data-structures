2f934ca789e8a57d84bece4fbee902731e340415
Dave Moten <davidmoten@gmail.com>
2013-07-22 20:25:18 +1000

    add LogStructuredStorage methods

--- src/main/java/com/github/davidmoten/structures/btree/LogStructuredStorage.java
+++ src/main/java/com/github/davidmoten/structures/btree/LogStructuredStorage.java
@@ -1,15 +1,33 @@
 package com.github.davidmoten.structures.btree;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class LogStructuredStorage {
 
-	public LogStructuredStorage(File directory, long maxFileSize) {
+	private final int currentFileNo = 1;
+	private final RandomAccessFile currentFile;
 
+	public LogStructuredStorage(File directory, long maxFileSize) {
+		try {
+			currentFile = new RandomAccessFile(new File(directory,
+					currentFileNo + ".lss"), "rws");
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
-	public void append(HasPosition... hasPosition) {
-
+	public long append(byte[] bytes) {
+		try {
+			long pos = currentFile.length();
+			currentFile.seek(pos);
+			currentFile.write(bytes);
+			return pos;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
