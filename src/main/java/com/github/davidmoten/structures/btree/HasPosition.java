8c30843ba692ad0da77654719a7e6e06a8e78ed6
Dave Moten <davidmoten@gmail.com>
2013-07-22 20:19:32 +1000

    add LogStructuredStorage

---
+++ src/main/java/com/github/davidmoten/structures/btree/HasPosition.java
@@ -0,0 +1,9 @@
+package com.github.davidmoten.structures.btree;
+
+public interface HasPosition {
+
+	void setPosition(Position position);
+
+	Position getPosition();
+
+}
---
+++ src/main/java/com/github/davidmoten/structures/btree/LogStructuredStorage.java
@@ -0,0 +1,15 @@
+package com.github.davidmoten.structures.btree;
+
+import java.io.File;
+
+public class LogStructuredStorage {
+
+	public LogStructuredStorage(File directory, long maxFileSize) {
+
+	}
+
+	public void append(HasPosition... hasPosition) {
+
+	}
+
+}
